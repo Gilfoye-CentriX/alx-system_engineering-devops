6e162a83a96977c24f2044f059602e6950680d31
Gilfoye-CentriX <125440612+Gilfoye-CentriX@users.noreply.github.com>
2023-11-29 22:51:00 +0200

    Web Server

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,16 @@
+# add stable version of nginx
+exec { 'add nginx stable repo':
+  command => 'sudo add-apt-repository ppa:nginx/stable',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
+# update software packages list
+exec { 'update packages':
+  command => 'apt-get update',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
+# install nginx
+package { 'nginx':
+  ensure     => 'installed',
+}
