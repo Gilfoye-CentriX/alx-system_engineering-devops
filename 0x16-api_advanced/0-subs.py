756c9e6d6b4cec1cd02fb7f31d4c212bdaa16dd4
Gilfoye-CentriX <125440612+Gilfoye-CentriX@users.noreply.github.com>
2024-01-16 13:58:26 +0200

    api advanced correction

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-"""
-Function that queries the Reddit API and returns the number of subscribers
-(not active users, total subscribers) for a given subreddit
-"""
-from requests import get
+"""function that queries the Reddit API and returns the number of subscribers
+for a given subreddit"""
+
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Suscribers by subreddit
-    """
-    headers = {"user-agent": "kyeeh"}
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    try:
-        req_data = get(url, headers=headers).json()
-        subscriber_count = req_data["data"]["subscribers"]
-        return (subscriber_count)
-    except Exception:
+    """function that queries the Reddit API and returns the number of
+    subscribers for a given subreddit"""
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data.get('data').get('subscribers')
+    else:
         return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
-"""
-Function that queries the Reddit API and prints the titles
-of the first 10 hot posts listed for a given subreddit.
-"""
-from requests import get
+"""function that queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit."""
 
 
 def top_ten(subreddit):
-    """
-    The first 10 hot posts by subreddit
-    """
-    headers = {"user-agent": "kyeeh"}
-    url = "https://api.reddit.com/r/{}/hot?limit=10".format(subreddit)
+    """function that queries the Reddit API and prints the titles
+    of the first 10 hot posts listed for a given subreddit."""
+    import requests
+    import sys
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
     try:
-        req_data = get(url, headers=headers, allow_redirects=False).json()
-        hot_posts = req_data["data"]["children"]
-        for post in hot_posts:
-            print(post['data']['title'])
+        response = requests.get(url, headers=headers,
+                                allow_redirects=False)
+        if response.status_code == 200:
+            children = response.json().get('data').get('children')
+            for i in range(10):
+                print(children[i].get('data').get('title'))
+        else:
+            print("None")
     except Exception:
         print("None")
